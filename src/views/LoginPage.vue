d63464166a9ed5c35c5237dcb76fc3506e9eb174
zainchen <chen77078@gmail.com>
2025-07-02 17:37:54 +0800

    tailwind重写login page

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,35 +1,82 @@
 <template>
-  <div class="login">
-    <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" class="login-form">
-      <h3 class="title">{{ title }}</h3>
+  <div class="flex min-h-screen items-center justify-center bg-[url(@/assets/images/login-background.jpg)] bg-cover bg-center bg-no-repeat">
+    <el-form
+      ref="loginFormRef"
+      :model="loginForm"
+      :rules="loginFormRules"
+      class="w-96 rounded-2xl bg-white/60 p-6 pb-5 shadow-lg backdrop-blur-sm">
+      <h3 class="mb-8 text-center text-xl font-medium text-black">{{ title }}</h3>
       <el-form-item prop="username">
-        <el-input v-model="loginForm.username" type="text" size="large" auto-complete="off" placeholder="账号">
-          <template #prefix><svg-icon icon-class="user" class="el-input__icon input-icon" /></template>
+        <el-input
+          v-model="loginForm.username"
+          type="text"
+          size="large"
+          auto-complete="off"
+          placeholder="账号"
+          class="h-10">
+          <template #prefix>
+            <svg-icon
+              icon-class="user"
+              class="ml-0 h-10 w-3.5" />
+          </template>
         </el-input>
       </el-form-item>
       <el-form-item prop="password">
-        <el-input v-model="loginForm.password" type="password" size="large" auto-complete="off" placeholder="密码" @keyup.enter="handleLogin">
-          <template #prefix><svg-icon icon-class="password" class="el-input__icon input-icon" /></template>
+        <el-input
+          v-model="loginForm.password"
+          type="password"
+          size="large"
+          auto-complete="off"
+          placeholder="密码"
+          class="h-10"
+          @keyup.enter="handleLogin">
+          <template #prefix>
+            <svg-icon
+              icon-class="password"
+              class="ml-0 h-10 w-3.5" />
+          </template>
         </el-input>
       </el-form-item>
       <el-form-item prop="captcha">
-        <el-input v-model="loginForm.captcha" size="large" auto-complete="off" placeholder="验证码" style="width: 63%" @keyup.enter="handleLogin">
-          <template #prefix><svg-icon icon-class="validCode" class="el-input__icon input-icon" /></template>
+        <el-input
+          v-model="loginForm.captcha"
+          size="large"
+          auto-complete="off"
+          placeholder="验证码"
+          class="h-10 flex-1"
+          @keyup.enter="handleLogin">
+          <template #prefix>
+            <svg-icon
+              icon-class="validCode"
+              class="ml-0 h-10 w-3.5" />
+          </template>
         </el-input>
-        <div class="login-captcha">
-          <img :src="captchaImgUrl" @click="getCaptcha" class="login-captcha-img" />
+        <div class="h-10 w-32">
+          <img
+            :src="captchaImgUrl"
+            @click="getCaptcha"
+            class="h-10 cursor-pointer pl-3" />
         </div>
       </el-form-item>
-      <el-checkbox v-model="loginForm.rememberMe" style="margin: 0px 0px 25px 0px">记住密码</el-checkbox>
-      <el-form-item style="width: 100%">
-        <el-button :loading="loading" size="large" type="primary" style="width: 100%" @click.prevent="handleLogin(loginFormRef)">
+      <el-checkbox
+        v-model="loginForm.rememberMe"
+        style="margin: 0px 0px 25px 0px">
+        记住密码
+      </el-checkbox>
+      <el-form-item class="mb-0 w-full">
+        <el-button
+          :loading="loading"
+          size="large"
+          type="primary"
+          class="w-full"
+          @click.prevent="handleLogin(loginFormRef)">
           <span v-if="!loading">登 录</span>
           <span v-else>登 录 中...</span>
         </el-button>
       </el-form-item>
     </el-form>
 
-    <div class="el-login-footer">
+    <div class="font-arial fixed bottom-0 h-10 w-full text-center text-xs leading-10 tracking-wide text-white">
       <span>No Copyright.</span>
     </div>
   </div>
@@ -169,69 +216,12 @@ getCookie()
 getCaptcha()
 </script>
 
-<style lang="scss" scoped>
-.login {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  background-image: url('../assets/images/login-background.jpg');
-  background-size: cover;
-}
-.title {
-  margin: 0px auto 30px auto;
-  text-align: center;
-  color: #000000;
+<style scoped>
+:deep(.el-input__inner) {
+  height: 40px !important;
 }
 
-.login-form {
-  border-radius: 6px;
-  background: rgba(255, 255, 255, 0.6);
-  backdrop-filter: blur(6px);
-  border-radius: 15px;
-  width: 400px;
-  padding: 25px 25px 5px 25px;
-  z-index: 1;
-  .el-input {
-    height: 40px;
-    input {
-      height: 40px;
-    }
-  }
-  .input-icon {
-    height: 39px;
-    width: 14px;
-    margin-left: 0px;
-  }
-}
-.login-tip {
-  font-size: 13px;
-  text-align: center;
-  color: #bfbfbf;
-}
-.login-captcha {
-  width: 33%;
-  height: 40px;
-  float: right;
-  img {
-    cursor: pointer;
-    vertical-align: middle;
-  }
-}
-.el-login-footer {
-  height: 40px;
-  line-height: 40px;
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  text-align: center;
-  color: #fff;
-  font-family: Arial;
-  font-size: 12px;
-  letter-spacing: 1px;
-}
-.login-captcha-img {
-  height: 40px;
-  padding-left: 12px;
+:deep(.el-input__prefix) {
+  align-items: center;
 }
 </style>
